952d79d69883c331894325d56b903b6513cf3100
timolee <timo.li.icon@gmail.com>
2014-05-01 16:07:20 +0800

    xxx

---
+++ web/src/main/java/com/ireald/wp/shiro/ShiroDbRealm.java
@@ -0,0 +1,26 @@
+package com.ireald.wp.shiro;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Timo on 2014/5/1.
+ */
+@Component("shiroDbRealm")
+public class ShiroDbRealm extends AuthorizingRealm {
+
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
